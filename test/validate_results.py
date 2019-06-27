ab47c2b78e4b7dc1a6eb782589f75d4fbd6753de
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-06-26 17:04:34 -0700

    Test

--- test/validate_results.py
+++ test/validate_results.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import re
 import math
 import cv2
 import numpy as np
@@ -37,6 +38,14 @@ if ',' in sys.argv[1]:
         file_endings.append(ending.strip())
 else:
     file_endings = [sys.argv[1].strip()]
+
+# Check for a dataset filter and ensure it's not an empty string (white space counts)
+dataset_filter = None
+if argc > 2:
+    dataset_filter = sys.argv[2]
+    dataset_filter_len = len(dataset_filter)
+    if dataset_filter_len <= 0:
+        dataset_filter = None
 
 def find_file_match(folder, end):
     """Locates a file in the specified folder that has the matching ending.
@@ -81,75 +90,124 @@ def find_file_match(folder, end):
 
     return None
 
+def find_filtered_folders(folder, regex_filter=None):
+    """Finds subfolders that match the filter
+    Args:
+        folder(str): the folder to iterate over
+        regex_filter(str): optional regular expression used to filter out subfolders
+    Return:
+        A list of matched folders or None if no folders were found and/or matched
+    Notes:
+        If regex_filter is None then all subfolders are considered a match
+    """
+    found = []
+
+    for name in os.listdir(folder):
+        # Skip over special folders and hidden names
+        name_len = len(name)
+        if name_len >= 1 and name[0] == '.':
+            continue
+
+        full_name = os.path.join(folder, name)
+        if os.path.isdir(full_name):
+            if not regex_filter is None:
+                match = re.search(regex_filter, full_name)
+                if not match is None:
+                    found.append(full_name)
+            else:
+                found.append(full_name)
+
+    found_len = len(found)
+    return found if not found_len <= 0 else None
+
+# Find subfolders if they're specified. Having no subfolders, with a filter specified, is not considered an error
+if not dataset_filter is None:
+    filtered_folders = find_filtered_folders(compare_folder, dataset_filter)
+    if filtered_folders is None:
+        filtered_folders = find_filtered_folders(datasets_folder, dataset_filter)
+    if filtered_folders is None:
+        filtered_folders = [None]
+else:
+    filtered_folders = [None]
+
+# Loop through everything
+filtered_folder_range = range(0, len(filtered_folders))
 for one_end in file_endings:
-    # Find the file with the correct name
-    master = find_file_match(compare_folder, one_end)
-    source = find_file_match(datasets_folder, one_end)
+    # If we have subfolders, we loop through those
+    for folder_idx in filtered_folder_range:
+        # Find the file with the correct name
+        sub_folder = filtered_folders[folder_idx]
 
-    print("Master image: " + str(master))
-    print("Source image: " + str(source))
+        match_folder = compare_folder if sub_folder is None else os.path.join(compare_folder, sub_folder)
+        master = find_file_match(match_folder, one_end)
 
-    if master is None:
-        raise RuntimeError("Missing the comparison files used to validate results: " + str(one_end))
-    if source is None:
-        raise RuntimeError("Missing the resulting files from the dataset: " + str(one_end))
+        match_folder = datasets_folder if sub_folder is None else os.path.join(datasets_folder, sub_folder)
+        source = find_file_match(match_folder, one_end)
 
-    # Check file sizes
-    master_size = os.path.getsize(master)
-    source_size = os.path.getsize(source)
-    if master_size <= 0 and not source_size <= 0:
-        raise RuntimeError("Generated file is not empty like comparison file: " + source + " vs " + master)
-    if not master_size == 0:
-        diff = abs(master_size - source_size)
-        if not diff == 0 and float(diff)/float(master_size) > FILE_SIZE_MAX_DIFF_FRACTION:
-            raise RuntimeError("File size difference exceeds limit of " + FILE_SIZE_MAX_DIFF_FRACTION + ": " + source + " vs " + master)
-    if master_size == 0 or source_size == 0:
-        print("Success compare empty files (" + one_end + "): " + source + " vs " + master)
-        continue
+        print("Master file: " + str(master))
+        print("Source file: " + str(source))
 
-    # Check file types
-    ext = os.path.splitext(master)
-    if not ext:
-        print("Success compare extension-less files (" + one_end + "): " + source + " vs " + master)
-        continue
+        if master is None:
+            raise RuntimeError("Missing the comparison files used to validate results: " + str(one_end))
+        if source is None:
+            raise RuntimeError("Missing the resulting files from the dataset: " + str(one_end))
 
-    if not (ext == ".tif" or ext == "png"):
-        print("Success. No futher tests for files (" + one_end + "): " + source + " vs " + master)
-        continue
+        # Check file sizes
+        master_size = os.path.getsize(master)
+        source_size = os.path.getsize(source)
+        if master_size <= 0 and not source_size <= 0:
+            raise RuntimeError("Generated file is not empty like comparison file: " + source + " vs " + master)
+        if not master_size == 0:
+            diff = abs(master_size - source_size)
+            if not diff == 0 and float(diff)/float(master_size) > FILE_SIZE_MAX_DIFF_FRACTION:
+                raise RuntimeError("File size difference exceeds limit of " + FILE_SIZE_MAX_DIFF_FRACTION + ": " + source + " vs " + master)
+        if master_size == 0 or source_size == 0:
+            print("Success compare empty files (" + one_end + "): " + source + " vs " + master)
+            continue
 
-    im_mas = cv2.imread(master)
-    im_src = cv2.imread(source)
+        # Check file types
+        ext = os.path.splitext(master)
+        if not ext:
+            print("Success compare extension-less files (" + one_end + "): " + source + " vs " + master)
+            continue
 
-    if im_mas is None:
-        print("Master image was not loaded: '" + master + "'")
-        exit(1)
-    if im_src is None:
-        print("Dataset image was not loaded: '" + source + "'")
-        exit(1)
+        if not (ext == ".tif" or ext == "png"):
+            print("Success. No futher tests for files (" + one_end + "): " + source + " vs " + master)
+            continue
 
-    # We use a dict so that we can add better error handling later if desired
-    failures = {}
+        im_mas = cv2.imread(master)
+        im_src = cv2.imread(source)
 
-    # Check the image attributes
-    if not im_mas.shape == im_src.shape:
-        failures['image dimensions'] = True
+        if im_mas is None:
+            print("Master image was not loaded: '" + master + "'")
+            exit(1)
+        if im_src is None:
+            print("Source image was not loaded: '" + source + "'")
+            exit(1)
 
-    if 'image dimensions' not in failures:
-        # calculate the differences between the images and check that
-        diff = np.absolute(np.subtract(im_mas, im_src))
-        hist, _ = np.histogram(diff, 256, (0, 255))
+        # We use a dict so that we can add better error handling later if desired
+        failures = {}
 
-        start_idx = HIST_START_INDEX if HIST_START_INDEX < hist.size else 0
-        for idx in range(start_idx, hist.size):
-            if hist[idx] > HIST_BIN_MAX:
-                failures['image differences'] = True
-                break
+        # Check the image attributes
+        if not im_mas.shape == im_src.shape:
+            failures['image dimensions'] = True
 
-    # Report any errors back
-    failures_len = len(failures)
-    if failures_len > 0:
-        print("We have " + str(failures_len) + "errors detected for files (" + one_end + "): " + source + " vs " + master)
-        errs = ', '.join(str(k) for k in failures.keys())
-        raise RuntimeError("Errors found: %s" % errs)
+        if 'image dimensions' not in failures:
+            # calculate the differences between the images and check that
+            diff = np.absolute(np.subtract(im_mas, im_src))
+            hist, _ = np.histogram(diff, 256, (0, 255))
+
+            start_idx = HIST_START_INDEX if HIST_START_INDEX < hist.size else 0
+            for idx in range(start_idx, hist.size):
+                if hist[idx] > HIST_BIN_MAX:
+                    failures['image differences'] = True
+                    break
+
+        # Report any errors back
+        failures_len = len(failures)
+        if failures_len > 0:
+            print("We have " + str(failures_len) + "errors detected for files (" + one_end + "): " + source + " vs " + master)
+            errs = ', '.join(str(k) for k in failures.keys())
+            raise RuntimeError("Errors found: %s" % errs)
 
 print("Test has run successfully")
